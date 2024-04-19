20ba2a65b2a1a6b0823111d9bb4dac08fea4298d
Luca Romanò <luca.romano2500@gmail.com>
2024-04-19 16:56:49 +0200

    Added front-end visualizer

--- visualizer/controller.py
+++ visualizer/controller.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify
+import os
 
 app = Flask(__name__)
 
-# Route per la homepage
+template_dir = os.path.abspath('front-end')
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/message', methods=['POST'])
+def receive_message():
+    return jsonify({'response': 'Message received from JavaScript!'})
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# Route per ricevere i dati dal form
-@app.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        return render_template('submitted.html', name=name, email=email)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
