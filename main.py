c49a96718082954163bccca7e696f8b2eabe2211
noahSantarossa <noahsantarossa@gmail.com>
2024-09-03 14:21:21 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,71 +1,57 @@
 import sys
+import os
 import SchedIO
-import Task,subprocess
+import Task
+import subprocess
+import time
+import threading
+from Visualizer import create_graph
+
+def delete_files_after_delay(file_paths, delay):
+    """Delete the specified files after a delay in seconds."""
+    time.sleep(delay)
+    for file_path in file_paths:
+        try:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                print(f"Deleted {file_path}")
+            else:
+                print(f"File {file_path} does not exist.")
+        except Exception as e:
+            print(f"Error deleting {file_path}: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
+        # Use Schedsim "main.py" execution
         input_path = sys.argv[1]
         output_path = sys.argv[2]
         scheduler = SchedIO.import_file(input_path, output_path)
-        '''
-        print("Do you want to add some seconds?")
-        while True:
-            seconds = input()
-            if seconds.lower() == "next":
-                print("Skipping the execution")
-                break  # Exits the loop
-            try:
-                seconds = int(seconds)
-                if 0 <= seconds <= 100:
-                    scheduler.add_time(seconds)
-                    break  # Exits the loop
-                else:
-                    raise ValueError("The number of seconds must be between 0 and 100")
-            except ValueError:
-                print("Invalid value, please try again or type 'Next' to skip the execution:")
-
-        '''
+        
         scheduler.execute()
         
-
-        #scheduler.execute()
-
-
-        # Chiamata al controller come se fosse da riga di comando
-        #command = f'python3 controller.py {input_path} {output_path}'
-        #subprocess.run(command, shell=True, check=True)
-        '''while(True):
-            real_time = input()
-            real_time = bool(real_time)
-            _type = input()
-            _id = input()
-            _id = int(_id)
-            period = input()
-            if period == 'None':
-                period = None
-            else:
-                period = int(period)
-            activation = input()
-            activation = int(activation)
-            deadline = input()
-            deadline = int(deadline)
-            wcet = input()
-            wcet = int(wcet)
-            new_task = Task.Task(real_time, _type, _id, period, activation, deadline, wcet)
-            scheduler.new_task(new_task)'''
-
-        #new_task = Task.Task(True, 'periodic', 5, 20, 0, 100, 5)
+        # Try the new implemented functionality
+        
+        # SPORADIC
         new_task = Task.Task(True, 'sporadic', 5, None, 15, 100, 5)
-        #new_task = Task.Task(False, 'sporadic', 4, None, 10, None, 15)
-        #scheduler.new_task(new_task)
+        scheduler.new_task(new_task)
 
-        new_task = Task.Task(True, 'sporadic', 11, None, 15, 100, 5)
-        #new_task = Task.Task(True,'sporadic',6,None, 0,100,10)
+        # ADD_TIME
+        scheduler.add_time(40)
+        
+        # PERIODIC
+        new_task = Task.Task(True, 'periodic', 8, 10, None, 100, 5)
         scheduler.new_task(new_task)
         
-
-        #scheduler.add_time(40)
-
+        # CREATE GRAPH
+        output_image_path = 'visualizer/static/out2.png'
+        create_graph(output_image_path, scheduler.start, scheduler.end, 1)
+        
+        # Delete the created temporany files
+        file_to_delete = [output_image_path]
+        delete_delay = 10
+        print(f"Files will be deleted after {delete_delay} seconds...")
+        deletion_thread = threading.Thread(target=delete_files_after_delay, args=(file_to_delete, delete_delay))
+        deletion_thread.start()
 
     else:
         raise Exception(
