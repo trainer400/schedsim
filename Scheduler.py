ef2f8a43c3ca6fd075dfe2521ee422553306983f
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-10-27 17:53:28 +0100

    [Scheduler][RateMonotonic] Added new_task and add_time methods

--- Scheduler.py
+++ Scheduler.py
@@ -1167,10 +1167,79 @@ class RateMonotonic(Preemptive):
         self.compute(time, count)
 
     def new_task(self, new_task):
-        pass
+        '''
+            The function adds a new task to the scheduling, identifying the last available snapshot and 
+            restoring the correct events lists.
+        '''
+        time = self.start
+        count = 0
+
+        # Here you can add the code to choose between different cores:
+        new_task.core = self.cores[0].id
+        # ------------------------------- #
+        if new_task.type == 'sporadic' and new_task.activation > self.start:
+            time = new_task.activation
+            pos = search_pos(self, time - 1)
+
+            # Restore the last snapshot that can be used to insert the new task
+            self.finish_events = copy.deepcopy(self.finish_events_list[pos])
+            self.deadline_events = copy.deepcopy(
+                self.deadline_events_list[pos])
+            self.arrival_events = copy.deepcopy(self.arrival_events_list[pos])
+            self.start_events = copy.deepcopy(self.start_events_list[pos])
+            self.executing = copy.deepcopy(self.executing_list[pos])
+
+            if self.executing:
+                self.executing = self.start_events[0]
+
+            # Add the new task and create the activation event
+            self.tasks.append(new_task)
+            new_task.init = new_task.activation
+            event = SchedEvent.ScheduleEvent(
+                new_task.activation, new_task, SchedEvent.EventType.activation.value, self.event_id)
+            self.event_id += 1
+
+            # Add the event inside the arrivals and sort them
+            for p in range(pos + 1):
+                self.arrival_events_list[p].append(copy.deepcopy(event))
+                self.arrival_events_list[p].sort(key=lambda x: x.timestamp)
+
+            self.arrival_events.append(copy.deepcopy(event))
+            self.arrival_events.sort(key=lambda x: x.timestamp)
+            time = self.time_list[pos] + 1
+            delete(self, time)
+        else:
+            reset(self)
+            self.tasks.append(new_task)
+            self.arrival_events = self.get_all_arrivals()
+            count = self.size - 1
+
+        self.output_file.clean(time)
+        self.compute(time, count)
 
     def add_time(self, add_time):
-        pass
+        '''
+            Adds time to the simulation restoring the last available snapshot
+            @param add_time the simulation time to be added at the end
+        '''
+        self.add_arrivals(self.end, self.end + add_time)
+
+        # Restore the last available snapshot
+        pos = search_pos(self, self.end - 1)
+        self.finish_events = copy.deepcopy(self.finish_events_list[pos])
+        self.deadline_events = copy.deepcopy(self.deadline_events_list[pos])
+        self.arrival_events = copy.deepcopy(self.arrival_events_list[pos])
+        self.start_events = copy.deepcopy(self.start_events_list[pos])
+        self.executing = copy.deepcopy(self.executing_list[pos])
+        if self.executing:
+            self.executing = self.start_events[0]
+
+        # Recompute with the new added time
+        self.end += add_time
+        time = self.time_list[pos] + 1
+        delete(self, time)
+        self.output_file.clean(time)
+        self.compute(time, self.start)
 
     def terminate(self):
         self.output_file.terminate_write()
